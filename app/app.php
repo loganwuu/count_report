370771a3231c27ccc344e125ab16d50f58e842e0
Logan <student@epicodus.com>
2015-08-14 11:00:06 -0700

    finish view_count.html.twig

--- app/app.php
+++ app/app.php
@@ -26,7 +26,9 @@
     $app->get("/view_count", function() use($app) {
         $my_RepeatCounter = new RepeatCounter;
         $repeat_number = $my_RepeatCounter->countRepeats($_GET['word'], $_GET['string']);
-        return $app['twig']->render('view_count.html.twig', array('result' => $repeat_number));
+        $word = $_GET['word'];
+        $string = $_GET['string'];
+        return $app['twig']->render('view_count.html.twig', array('result' => $repeat_number, 'word' => $word, 'string' => $string));
     });
 
     return $app;
